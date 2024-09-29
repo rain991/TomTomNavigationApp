da66b62ba1f5c974718ef58be7c93bdfdada86df
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-09-29 22:48:23 +0200

    added implementation of TomTomMapView. Now it handles map fragment and updates data correctly

--- app/src/main/java/com/savenko/tomtomnavigationapp/presentation/TomTomMapView.kt
+++ app/src/main/java/com/savenko/tomtomnavigationapp/presentation/TomTomMapView.kt
@@ -1,6 +1,7 @@
 package com.savenko.tomtomnavigationapp.presentation
 
 import android.view.View
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -12,8 +13,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.FragmentActivity
 import com.savenko.tomtomnavigationapp.BuildConfig
+import com.savenko.tomtomnavigationapp.R
 import com.tomtom.sdk.location.GeoPoint
-
 import com.tomtom.sdk.map.display.MapOptions
 import com.tomtom.sdk.map.display.image.ImageFactory
 import com.tomtom.sdk.map.display.marker.MarkerOptions
@@ -35,4 +36,34 @@ fun TomTomMapView(
     val mapOptions = MapOptions(
         mapKey = BuildConfig.TOMTOM_API_KEY,
     )
+    LaunchedEffect(Unit) {
+        createTomTomMapFragment(fragmentManager, mapOptions) { fragment ->
+            mapFragment = fragment
+            isMapFragmentReady = true
+        }
+    }
+    Box(modifier) {
+        if (isMapFragmentReady) {
+            AndroidView(
+                factory = {
+                    mapFragment?.requireView() ?: View(it)
+                },
+                update = {
+                    mapFragment?.getMapAsync { tomtomMap ->
+                        val markerOptions = MarkerOptions(
+                            coordinate = GeoPoint(
+                                latitude = 49.185756979112135,
+                                longitude = 28.489548259118568
+                            ),
+                            pinImage = ImageFactory.fromResource(R.drawable.location_pin_solid),
+                            pinIconImage = ImageFactory.fromResource(R.drawable.location_dot_solid),
+                        )
+                        tomtomMap.addMarker(markerOptions)
+                        tomtomMap.addMarkerClickListener { }
+                        // tomtomMap.addMapClickListener {  }
+                    }
+                }
+            )
+        }
+    }
 }
